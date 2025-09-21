e6de181492fc8dfb171a5158bf0e3c5ad7b013e5
Hassan-Ali0 <hassanali031935@gmail.com>
2025-09-21 16:17:51 +0500

    add conditional routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Section1 from './components/Section1'
@@ -13,26 +13,41 @@ import Product from './components/Product'
 import Pdp from './pages/Pdp'
 import Login from './pages/Login'
 import Rejister from './pages/Rejister'
+import { AuthContext } from './context/AuthContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let { user } = useContext(AuthContext)
+
+  if(user){
+    console.log("yes")
+  }else{
+    
+    console.log("no")
+  }
 
   return (
     <>
       <div className='h-[100vh] w-[100vw] bg-black overflow-y-auto main-div'>
         <Routes>
-          <Route path='/' element={<Landingpage/>}/>
-          <Route path='/auth' element={<Auth/>}>
-            <Route index element={<Login/>} />
-            <Route index path='/auth/login' element={<Login/>} />
-            <Route path='/auth/rejister' element={<Rejister/>}/>
-          </Route>
-          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/' element={<Landingpage />} />
+          {(!user) ?
+            <Route path='/auth' element={<Auth />}>
+              <Route index element={<Login />} />
+              <Route index path='/auth/login' element={<Login />} />
+              <Route path='/auth/rejister' element={<Rejister />} />
+            </Route>
+            :
+            <Route path='/auth' element={<><Header/> <h1 className='text-gray-50 text-[5em] text-center mt-15'>You are ALready Login</h1></>}>
+              <Route index path='/auth/*' element={<><Header/> <h1 className='text-gray-50 text-[5em] text-center mt-15'>You are ALready Login</h1></>} />
+            </Route>
+          }
+          <Route path='/cart' element={<Cart />} />
           <Route path='/products'  >
             <Route index element={<Products />} />
-            <Route path=':id' element={<Pdp/>} />
+            <Route path=':id' element={<Pdp />} />
           </Route>
-          <Route path='*' element={<Page404/>}/>
+          <Route path='*' element={<Page404 />} />
         </Routes>
       </div>
 
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,34 +7,23 @@ const Product = memo(({ product }) => {
   const { cart, setCart } = useContext(CartContext)
 
 
-  // let box = useRef(null)
-  // let addBtn = useRef(null)
-  // let [isshowBtn, SetisShowBtn] = useState(false)
-
-  // let showBtn = (e) => {
-  //   SetisShowBtn(true)
-  // }
-
-  // let hideBtn = (e) => {
-  //   SetisShowBtn(false)
-  // }
-
   return (
     <>
       {product ?
-        <div className='flex flex-col gap-3 h-[400px] w-[320px] product-div'>
-          <div className='h-[75%] relative flex flex-col items-center justify-center bg-neutral-900 overflow-hidden'>
+        <div className='flex flex-col items-center  gap-3 h-[400px] w-[320px] product-div '>
+          <div className='h-[75%] w-full lg:relative flex flex-col items-center justify-center bg-neutral-900 overflow-hidden'>
             <Link to={`/products/${product?.id}`} className='h-[80%] product-image '>
               <img
                 className='h-full w-auto '
                 src={product?.image} alt="product-image" />
             </Link>
-            {true ? <button onClick={() => { setCart([...cart, 'abc']) }} className='h-[45px] w-full add-btn  opacity-0 invisible bg-gray-50 absolute right-0 bottom-0 text-[1em] font-bold [transition:all_0.5s_ease-in-out] cursor-pointer'>ADD TO CART</button> : null}
+            {true ? <button onClick={() => { setCart([...cart, 'abc']) }} className='h-[45px] hidden lg:block w-full add-btn  lg:opacity-0 lg:invisible bg-gray-50 lg:absolute right-0 bottom-0 text-[1em] font-bold [transition:all_0.5s_ease-in-out] cursor-pointer'>ADD TO CART</button> : null}
           </div>
           <Link to={`/products/${product?.id}`} className='txt-div'>
             <h2 className='text-gray-50 text-[1.4em] font-semibold text-center product-title' >{product?.title}</h2>
             <p className='text-gray-300 text-[1.2em] text-center product-price'>{`$${product?.price}`}</p>
           </Link>
+          {true ? <button onClick={() => { setCart([...cart, 'abc']) }} className='h-[45px] w-[120px] add-btn lg:hidden  bg-gray-50  text-[1em] font-bold  cursor-pointer mt-1'>ADD TO CART</button> : null}
         </div>
         :
         <h2 className='text-gray-50'>No Product</h2>
