890d80537e88b4e482cdb2eaf5e905d3c41ec90e
Hassan-Ali0 <hassanali031935@gmail.com>
2025-10-15 12:22:54 +0500

    add some cards

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,49 +1,57 @@
 import React from 'react'
 
 const BlogSection = () => {
-  return (
-    <div className='h-auto w-full flex justify-center'>
-        <div className='h-full w-full max-w-[1400px] py-[30px] px-[20px] flex flex-wrap justify-center gap-10'>
-            <div className='h-auto w-[420px] flex flex-col items-center gap-5'>
-                <img src="/images/blog1.webp" alt="" />
-                <div>
-                    <span className='text-red-500'>Blog -</span>
-                    <span className='text-gray-500'> July 2,2018 -</span>
-                    <span className='text-gray-500'> by Sakin</span>
+    return (
+        <div className='h-auto w-full flex justify-center'>
+            <div className='h-full w-full max-w-[1400px] py-[30px] px-[20px] flex flex-wrap justify-center gap-10'>
+                <div className='h-[110px] w-full flex flex-col justify-end items-center text-black dark:text-gray-50  p-[10px] box-border'>
+                    <div className='flex gap-4'>
+                        <span>_________</span>
+                        <span className='text-[1.2em] font-semibold text-neutral-700 dark:text-gray-300 text-center'>Latest Updates </span>
+                        <span>_________</span>
+                    </div>
+                    <h2 className='text-[2em] lg:text-[2.5em] text-black dark:text-gray-50 font-bold uppercase text-center'>From our blog</h2>
                 </div>
-                <h2 className='text-[1.8em] text-center font-bold'>Sum Up Your Fashion Experience</h2>
-                <p className='text-[1.3em] text-gray-500 text-center'>The only sign of commercial activity within the harbour, visible from the beach of the Great Isabel, is the square blunt end</p>
-                <button className='outline-0 text-[1.1em] font-semibold'>Continue Reading</button>
+                <div className='h-auto w-[420px] flex flex-col items-center gap-5'>
+                    <img src="/images/blog1.webp" alt="" />
+                    <div>
+                        <span className='text-red-500'>Blog -</span>
+                        <span className='text-gray-500'> July 2,2018 -</span>
+                        <span className='text-gray-500'> by Sakin</span>
+                    </div>
+                    <h2 className='text-[1.8em] text-center font-bold'>Sum Up Your Fashion Experience</h2>
+                    <p className='text-[1.3em] text-gray-500 text-center'>The only sign of commercial activity within the harbour, visible from the beach of the Great Isabel, is the square blunt end</p>
+                    <button className='outline-0 text-[1.1em] font-semibold'>Continue Reading</button>
 
-            </div>
-            <div className='h-auto w-[420px] flex flex-col items-center gap-5'>
-                <img src="/images/blog2.webp" alt="" />
-                <div>
-                    <span className='text-red-500'>Design, Style -</span>
-                    <span className='text-gray-500'> July 2,2018 -</span>
-                    <span className='text-gray-500'> by Sakin</span>
                 </div>
-                <h2 className='text-[1.8em] text-center font-bold'>This Is Why Fashion Is So Famous!</h2>
-                <p className='text-[1.3em] text-gray-500 text-center'>Human faces are very powerful. User Experience designer Aarron Walter describes in his book Designing for Emotion why we appreciate human faces</p>
-                <button className='outline-0 text-[1.1em] font-semibold'>Continue Reading</button>
+                <div className='h-auto w-[420px] flex flex-col items-center gap-5'>
+                    <img src="/images/blog2.webp" alt="" />
+                    <div>
+                        <span className='text-red-500'>Design, Style -</span>
+                        <span className='text-gray-500'> July 2,2018 -</span>
+                        <span className='text-gray-500'> by Sakin</span>
+                    </div>
+                    <h2 className='text-[1.8em] text-center font-bold'>This Is Why Fashion Is So Famous!</h2>
+                    <p className='text-[1.3em] text-gray-500 text-center'>Human faces are very powerful. User Experience designer Aarron Walter describes in his book Designing for Emotion why we appreciate human faces</p>
+                    <button className='outline-0 text-[1.1em] font-semibold'>Continue Reading</button>
 
-            </div>
-            <div className='h-auto w-[420px] flex flex-col items-center gap-5'>
-                <img src="/images/blog3.webp" alt="" />
-                <div>
-                    <span className='text-red-500'>Blog -</span>
-                    <span className='text-gray-500'> July 2,2018 -</span>
-                    <span className='text-gray-500'> by Sakin</span>
                 </div>
-                <h2 className='text-[1.8em] text-center font-bold'>5 Stereotypes About Fashion</h2>
-                <p className='text-[1.3em] text-gray-500 text-center'>Human faces are very powerful. User Experience designer Aarron Walter describes in his book Designing for Emotion why we appreciate human faces</p>
-                <button className='outline-0 text-[1.1em] font-semibold'>Continue Reading</button>
+                <div className='h-auto w-[420px] flex flex-col items-center gap-5'>
+                    <img src="/images/blog3.webp" alt="" />
+                    <div>
+                        <span className='text-red-500'>Blog -</span>
+                        <span className='text-gray-500'> July 2,2018 -</span>
+                        <span className='text-gray-500'> by Sakin</span>
+                    </div>
+                    <h2 className='text-[1.8em] text-center font-bold'>5 Stereotypes About Fashion</h2>
+                    <p className='text-[1.3em] text-gray-500 text-center'>Human faces are very powerful. User Experience designer Aarron Walter describes in his book Designing for Emotion why we appreciate human faces</p>
+                    <button className='outline-0 text-[1.1em] font-semibold'>Continue Reading</button>
 
+                </div>
             </div>
+
         </div>
-        
-    </div>
-  )
+    )
 }
 
 export default BlogSection
--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -25,10 +25,30 @@ const Landingpage = () => {
       <Section2 />
       <FpSection />
       <Fcsection />
-      <SpecialSection/>
-      <NewArrivals/>
-      <SubscribeSection/>
-      <BlogSection/>
+      <SpecialSection />
+      <NewArrivals />
+      <SubscribeSection />
+      <BlogSection />
+      <div className=' h-[500px] w-full flex justify-center'>
+        <div className='h-full w-full max-w-[1400px] flex flex-wrap justify-center items-center gap-10 px-[20px] py-[30px]'>
+          <div className='h-[220px] w-[300px] lg:w-[400px] flex flex-col gap-2 justify-center items-center border-gray-300 border-[1px]'>
+            <img src="/images/free-shipping.png" alt="free shiping" />
+            <h3 className='text-[1.6em] font-bold uppercase'>Free Shipping</h3>
+            <p className='text-[1.1em] text-gray-500'>on all orders over $49.00</p>
+          </div>
+          <div className='h-[220px] w-[300px] lg:w-[400px] flex flex-col gap-2 justify-center items-center border-gray-300 border-[1px]'>
+            <img src="/images/return.png" alt="15 days returns" />
+            <h3 className='text-[1.6em] font-bold uppercase'>15 Days Returns</h3>
+            <p className='text-[1.1em] text-gray-500'>moneyback guarantee</p>
+          </div>
+          <div className='h-[220px] w-[300px] lg:w-[400px] flex flex-col gap-2 justify-center items-center border-gray-300 border-[1px]'>
+            <img src="/images/secure.png" alt="secure checkout" />
+            <h3 className='text-[1.6em] font-bold uppercase'>Secure Checkout</h3>
+            <p className='text-[1.1em] text-gray-500'>100% protected by Paypal</p>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
